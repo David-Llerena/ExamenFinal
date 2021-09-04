3e574417c02b43567caabff87093a8df052942b2
david llerena <dllerena@espol.edu.ec>
2021-09-03 22:30:19 -0500

    Agregacion del proyecto

---
+++ amst_g7/android/app/src/main/java/com/example/amst_g7/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.amst_g7;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ amst_g7/lib/provider/google_sign_in.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInProvider extends ChangeNotifier{
+
+
+
+}
